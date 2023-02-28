8e5a330be976b1e29a3295b4ec1561a6055890fd
ksmtrv <smotrova-kristina@mail.ru>
2023-02-28 22:01:55 +0300

    280223

---
+++ net_task2_36/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace net_task2_36
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox2.Clear();
+                textBox3.Clear();   
+                string s = textBox1.Text;
+                textBox2.Text += System.IO.File.ReadAllText(s + ".txt", System.Text.Encoding.UTF8);
+                button2.Enabled = true;
+
+            } catch {
+                MessageBox.Show("Код ошибки: -1");
+                button2.Enabled= false;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            try {
+                int i = 0;
+                using (StreamReader r = new StreamReader(s + ".txt"))
+                {
+                    while (r.ReadLine() != null)
+                    {
+                        i++;
+                    }
+                }
+                textBox3.Text = Convert.ToString(i);
+            } catch {
+                MessageBox.Show("Код ошибки: -1");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
